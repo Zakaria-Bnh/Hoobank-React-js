60d5878b49c328ada5960f82fd9d29b66afcf69d
Zakaria ben <127519578+Zakaria-Bnh@users.noreply.github.com>
2023-08-14 11:24:27 +0100

    the project is finished and it up and running

---
+++ src/Components/Button.jsx
@@ -0,0 +1,8 @@
+
+const Button = ({styles}) => (
+    <button type='button' className={`w-fit py-4 px-6 bg-blue-gradient font-poppins font-medieum text-[18px] text-primary outline-none ${styles}`}>
+      Get Statred
+    </button>
+  )
+
+export default Button
